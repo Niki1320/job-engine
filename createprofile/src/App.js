568ebbdf89f808858f153c16e87cb93a03eb71bd
faizanpas <135027964+faizanpas@users.noreply.github.com>
2023-08-31 21:54:18 +0530

    Add files via upload

---
+++ createprofile/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navb from "./Navb";
+import MainContent from "./MainContent";
+
+function App(){
+  return (
+    <div>
+      <Navb/>
+      <MainContent />
+    </div>
+  )
+}
+export default App;
---
+++ createprofile/src/Navb.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./CSS/Navb.css";
+function Navb(){
+    return (
+        <div className="navb">
+
+        </div>
+    )
+}
+export default Navb;
